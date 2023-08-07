a3a1ca400d57f72b9bdab7b972d4e3a75237307b
Brian <brianc.contacto@gmail.com>
2023-08-06 23:19:55 -0400

    fix images

--- src/app/api/images/route.ts
+++ src/app/api/images/route.ts
@@ -3,19 +3,21 @@ import { getImages, getUserPresignedUrls } from "@/utils/uploadImage";
 import { NextResponse, NextRequest } from "next/server";
 
 export async function GET(request: Request) {
-  const images: string[] = [];
-
+  const images = await prisma.image.findMany({
+    include: {
+      category: true,
+    },
+  });
   if (!images || images?.length === 0) {
     return NextResponse.json([]);
   }
 
-  // const { error, urls } = await getImages(images?.map(({ url }) => url));
-  // if (error) {
-  //   return NextResponse.json([]);
-  // }
-  // images?.forEach((image, idx) => {
-  //   image.url = urls![idx];
-  // });
-  // console.log("RESPUESTA 2, ", images);
+  const { error, urls } = await getImages(images?.map(({ url }) => url));
+  if (error) {
+    return NextResponse.json([]);
+  }
+  images?.forEach((image, idx) => {
+    image.url = urls![idx];
+  });
   return NextResponse.json(images ?? []);
 }
