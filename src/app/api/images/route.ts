8fe266a7e94e8afed0f66a0e5ab2ae654192f32a
Brian <brianc.contacto@gmail.com>
2023-08-06 23:12:45 -0400

    fix

--- src/app/api/images/route.ts
+++ src/app/api/images/route.ts
@@ -3,47 +3,19 @@ import { getImages, getUserPresignedUrls } from "@/utils/uploadImage";
 import { NextResponse, NextRequest } from "next/server";
 
 export async function GET(request: Request) {
-  console.log("GET IMAGES");
-  let images: ({
-    category: {
-      id: string;
-      name: string;
-      createdAt: Date;
-      updatedAt: Date;
-      enabled: boolean;
-    };
-  } & {
-    id: string;
-    name: string;
-    url: string;
-    albumId: string | null;
-    userId: string;
-    categoryId: string;
-    createdAt: Date;
-    updatedAt: Date;
-  })[] = [];
-  try {
-    images = await prisma.image.findMany({
-      include: {
-        category: true,
-      },
-    });
-  } catch (error) {
-    console.log("ERROR ESPECIAL", error);
-  }
-  console.log("AQUI XD", images);
+  const images: string[] = [];
+
   if (!images || images?.length === 0) {
-    console.log("RESPUESTA 1");
     return NextResponse.json([]);
   }
 
-  const { error, urls } = await getImages(images?.map(({ url }) => url));
-  if (error) {
-    return NextResponse.json([]);
-  }
-  images?.forEach((image, idx) => {
-    image.url = urls![idx];
-  });
-  console.log("RESPUESTA 2, ", images);
+  // const { error, urls } = await getImages(images?.map(({ url }) => url));
+  // if (error) {
+  //   return NextResponse.json([]);
+  // }
+  // images?.forEach((image, idx) => {
+  //   image.url = urls![idx];
+  // });
+  // console.log("RESPUESTA 2, ", images);
   return NextResponse.json(images ?? []);
 }
