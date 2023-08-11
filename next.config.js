9525a9b4c8d58dac6dcfd248ef861d9c988b363f
Brian <brianc.contacto@gmail.com>
2023-08-10 21:48:41 -0400

    add cdn discord

--- next.config.js
+++ next.config.js
@@ -20,6 +20,12 @@ const nextConfig = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "cdn.discordapp.com",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
 };
